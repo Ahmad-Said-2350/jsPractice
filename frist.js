00235ba274eafa18b729454e516e8f0c3378b438
Ahmad-Said-2350 <mail.saydorrahman@gmail.com>
2026-01-26 12:46:25 +0600

    practice git added done

---
+++ frist.js
@@ -0,0 +1,267 @@
+// ---------------practice_1-----------------
+
+let dateOfBirth = 2007;
+let brothers = 4;
+let students = 4700;
+let marks = 89;
+let rooms = 5;
+let programmingTime = 14;
+let poneTotal = 33;
+
+// ---------------practice_2-----------------
+
+
+let age = 19;
+console.log(age);
+
+let booksToRead = 250;
+console.log(booksToRead);
+
+let wakeUpTime = 7;
+wakeUpTime = 6;
+console.log(wakeUpTime);
+
+let wastedTime = 3;
+console.log(wakeUpTime);
+
+let borrow = 40000;
+
+let goals = 6;
+console.log(goals);
+
+let pillow = 2;
+console.log(pillow);
+
+// ---------------practice_3-----------------
+
+let myName = "Ahmad Said";
+console.log(myName);
+
+let address = "Dhaka";
+console.log(address);
+
+let bestFriend = "Dhaka college"
+console.log(bestFriend);
+
+let fan = "Dipeeka"
+console.log(fan);
+
+let movieOfTheYear = "Yeh Jaweeni Hai Deeewani";
+console.log(movieOfTheYear);
+
+let hobby = "Writer";
+console.log(hobby);
+
+let color = "Blue";
+console.log(color);
+
+// ---------------practice_4-----------------
+
+let friendExamPass = true;
+console.log(friendExamPass);
+
+let crush = true;
+console.log(crush);
+
+let celebrating = false;
+console.log(celebrating);
+
+let isHappy = true;
+let isMoney = false;
+let isHungry = true;
+
+console.log(isHappy);
+console.log(isMoney);
+console.log(isHungry);
+
+let isStudyDone = false;
+console.log(isStudyDone);
+
+let isPoneCharged = false;
+console.log(isPoneCharged);
+
+let isDinnerDone = false;
+console.log(isDinnerDone);
+
+
+/*  
+undefined or null
+
+*/
+
+let noDefined;
+let usedNull = null;
+
+// ---------------practice_5-----------------
+
+let income = 100;
+let cost = 40;
+let remained = income - cost;
+console.log(remained);
+
+let pencil = 10;
+let usedPencil = 10 / 2;
+let remainedPencil = usedPencil;
+console.log(remainedPencil);
+
+let income2 = 60;
+let cost2 = 15;
+let remained2_1 = income2 / cost2;
+let remained2_2 = income2 % cost2;
+console.log(remained2_1);
+console.log(remained2_2);
+
+let string = "Hello";
+let string2 = "world";
+let finalString = string + string2;
+console.log(finalString);
+
+let rice = 153;
+let dal = 261;
+let totalRiceDal = rice + dal;
+console.log(totalRiceDal);
+
+let income3 = 500;
+let cost3 = 75;
+let remained3_1 = income3 / cost3;
+let remained3_2 = income3 % cost3;
+console.log(Number(remained3_1.toFixed(3)));
+console.log(remained3_2);
+
+let income4 = 8;
+let cost4 = 3;
+let remained4_1 = income4 / cost4 ;
+let remained4_2 = income4 % cost4;
+console.log(Number(remained4_1.toFixed(1) ));
+console.log(remained4_2);
+
+let string3 = "Bangla" ;
+let string3_2 = "desh" ;
+let finalString3 = string3 + string3_2;
+console.log(finalString3);
+
+let totalRice = 480;
+let oneDay = 4;
+let dayOfMonths = 30;
+
+let monthlyUsd = oneDay * dayOfMonths;
+let totalMonth = totalRice / monthlyUsd
+console.log(totalMonth);
+
+// ---------------practice_6-----------------
+
+let price = 555;
+console.log(typeof price);
+
+let learning = true;
+console.log(typeof learning);
+
+let megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+let temperature = 102.5;
+console.log(typeof temperature);
+
+let isHungry2 = true;
+console.log(typeof isHungry2);
+
+let isRice = true;
+console.log(typeof isRice);
+
+/*  
+undefined or null
+
+*/
+let noDefined2;
+let usedNull2 = null;
+console.log( typeof noDefined2);
+console.log(typeof usedNull2);
+
+// ---------------practice_7-----------------
+
+let age2 = 100;
+age2 = 17;
+console.log(age2);
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+let studyTime = 4;
+studyTime = 2;
+console.log(studyTime);
+
+let friendsCount = 3;
+friendsCount = 0;
+console.log(friendsCount);
+
+let chairsCount = 40;
+chairsCount = 38;
+chairsCount = 32;
+console.log(chairsCount);
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
+// ---------------practice_8-----------------
+
+// GPA SCORE
+let score = 4.3
+
+
+// for-border let radius1 = 1;
+// for-img let radius2 = 2;
+// for-color  let radius3 = 3;
+
+/*
+let dress = 0;
+*/
+
+// no way
+let giftMony = 500;
+
+// 0
+let studyTime2 = 4;
+
+// no send mony
+let sendMony = 0;
+
+// pay 40000 for Nahid and Sayan
+let pay = 40000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
