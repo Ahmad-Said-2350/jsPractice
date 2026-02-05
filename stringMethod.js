f6bad2c41d43b97a4a7b4a5356e17a12205bebe3
Ahmad-Said-2350 <mail.saydorrahman@gmail.com>
2026-02-05 21:30:55 +0600

    practice Ongoing

--- stringMethod.js
+++ stringMethod.js
@@ -19,3 +19,53 @@ console.log(nameOfStr.charAt(nameOfStr.length -1 )); */
 let language = "javascript";
 console.log(language.indexOf("rip"));
 
+
+// -------------------- practice_2 -------------------- 
+
+const email = "User@Example.Com";
+const toLowerConvert = email.toLowerCase();
+console.log(toLowerConvert);
+
+
+const greeting = "hElo WoRLd";
+const toLowerConvertPractice = greeting.toLowerCase();
+console.log(toLowerConvertPractice);
+
+
+let languageCheck = "javascript";
+
+if(languageCheck.toLowerCase().includes("script")){
+    console.log("Checking Done");
+}
+else{
+    console.log("Not included");
+}
+
+
+
+const textCheck = "NodeJs";
+
+const fristCharacter = textCheck.charAt(0);
+// const fristCharacter = textCheck[0];
+
+
+if(fristCharacter >= "A" && fristCharacter <= "Z"){
+    console.log("First character is uppercase");
+}
+else{
+    console.log("First character is not uppercase");
+
+}
+
+
+
+/*
+const textCheck = "NodeJs";
+
+if (textCheck[0] === textCheck[0].toUpperCase()) {
+    console.log("First character is uppercase");
+} else {
+    console.log("First character is NOT uppercase");
+}
+*/
+
